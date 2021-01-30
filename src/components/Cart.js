28df2d7d0a422303017a463ffb9401a579327a24
ArtemDid <58402562+ArtemDid@users.noreply.github.com>
2021-01-30 23:45:22 +0200

    Add files via upload

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -57,6 +57,7 @@ class Cart extends React.Component {
 
                             if (countBanana > 0 || countApple > 0 || countPapaya > 0)
                                 alert("Bought!")
+                                window.location.reload();
                         }} />
                     </div>
                 </div>
